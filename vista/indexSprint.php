bb5cd43e5d63e2dfab65eade2d14dfd9aa7007f3
Sandra G <saandrraagl@gmail.com>
2016-04-30 21:47:12 +0200

    Vista Sprint con estilo

---
+++ vista/indexSprint.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>Sprint</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="../estilo/estilo.css"/>
+    </head>
+    <body>
+        <h1>Hola</h1>
+        <h1>Sprint <?php ?></h1>
+        
+
+        <div id="backlog">
+            <h1>Backlog</h1>
+            
+        </div>
+        <div id="kanban">
+            
+            <div id="por_hacer"><h1>Por hacer</h1></div>
+            <div id="en_proceso"><h1>En proceso</h1></div>
+            <div id="terminado"><h1>Terminado</h1></div>
+            <div id="por_terminar"><h1>Por terminar</h1></div>
+        </div>
+        
+        <button>Volver</button>
+    </body>
+</html>
